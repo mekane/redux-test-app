e42694052f24c36c0a86c76659f8fef7d01cd913
Marty Kane <marty@pixotech.com>
2017-08-21 15:51:36 -0500

    Add some initial tests for adding items

--- src/app.js
+++ src/app.js
@@ -1,9 +1,37 @@
 const defaultState = {items: []};
 
 const testApp = (currentState, action) => {
-    if (typeof currentState !== 'object') {
+    if (currentStateIsBogus())
         return defaultState;
+
+    if (actionIsBogus())
+        return currentState;
+
+    const newState = {
+        items: []
+    };
+
+    if ( action.type === 'ADD_ITEM' ){
+        newState.items.push(action.text);
+    }
+
+    return newState;
+
+    function currentStateIsBogus() {
+        return hasBadType(currentState);
+    }
+
+    function actionIsBogus() {
+        return hasBadType(action);
     }
 };
+
+function hasBadType(argumentThatShouldBeAnObject) {
+    return isNotObject(argumentThatShouldBeAnObject);
+}
+
+function isNotObject(testObject) {
+    return (typeof testObject !== 'object');
+}
 
 module.exports = testApp;
--- test/app.spec.js
+++ test/app.spec.js
@@ -2,18 +2,52 @@ const expect = require('chai').expect;
 const testApp = require('../src/app.js');
 
 describe('The Test App root reducer', () => {
+    const defaultState = {
+        items: []
+    };
+
     it('should be a function', () => {
         expect(testApp).to.be.a('function');
     });
 
     it('should return the default state if called with none', () => {
-        const expectedState = {
-            items: []
-        };
+        const expectedState = defaultState;
         const actualState = testApp();
 
         expect(actualState).to.deep.equal(expectedState);
     });
-    
-    
+
+    it('should return the identical original state if called with no action', () => {
+        const originalState = {
+            items: ['foo']
+        };
+        const actualState = testApp(originalState);
+
+        expect(actualState).to.equal(originalState);
+    });
+
+    describe('adding items', () => {
+        it('should add the items array if it was missing', () => {
+            const expectedState = defaultState;
+            const actualState = testApp({}, {type: 'test'});
+
+            expect(actualState).to.deep.equal(expectedState);
+        });
+
+        it('should add items to the list if called with the ADD_ITEM action type', () => {
+            const addItemAction = {
+                type: 'ADD_ITEM',
+                text: 'test'
+            };
+
+            const expectedState = {
+                items: ['test']
+            };
+
+            const actualState = testApp(null, addItemAction);
+
+            expect(actualState).to.deep.equal(expectedState);
+        });
+    });
+
 });
