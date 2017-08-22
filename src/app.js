d5c66c107e6767cafa0631cbd9bb0ca650fc9a99
Marty Kane <marty@pixotech.com>
2017-08-22 13:54:55 -0500

    add items as objects with auto-incrementing ids, and in a by-ID map

--- src/app.js
+++ src/app.js
@@ -1,23 +1,29 @@
 'use strict';
-const defaultState = {items: []};
+const defaultState = () => {
+    return {
+        items: [],
+        itemsById: {},
+        lastItemId: 0
+    };
+};
 
 const testApp = (currentState, action) => {
     if (currentStateIsBogus())
-        return defaultState;
+        return defaultState();
 
     if (actionIsBogus())
         return currentState;
 
-    const newState = {
-        items: []
-    };
-
-    if (currentState.items && currentState.items.length)
-        newState.items = currentState.items.map(item => item);
+    const newState = copyState(currentState);
 
     if (action.type === 'ADD_ITEM') {
-        if (action.text)
-            newState.items.push(action.text);
+        if (action.text) {
+            const newId = lastItemId();
+            const newItem = {id: newId, text: action.text};
+            newState.items.push(newId);
+            newState.itemsById[newId] = newItem;
+            newState.lastItemId = newId;
+        }
     }
 
     return newState;
@@ -28,6 +34,24 @@ const testApp = (currentState, action) => {
 
     function actionIsBogus() {
         return hasBadType(action) || !action.type || !action.text;
+    }
+
+    function lastItemId() {
+        return (currentState.lastItemId || 0) + 1;
+    }
+
+    function copyState(oldState) {
+        if (!oldState) {
+            return defaultState();
+        }
+
+        const oldItems = oldState.items || [];
+
+        return {
+            items: oldItems.slice(),
+            itemsById: Object.assign({}, oldState.itemsById),
+            lastItemId: oldState.lastItemId || 0
+        }
     }
 };
 
--- test/app.spec.js
+++ test/app.spec.js
@@ -4,7 +4,9 @@ const testApp = require('../src/app.js');
 
 describe('The Test App root reducer', () => {
     const defaultState = {
-        items: []
+        items: [],
+        itemsById: {},
+        lastItemId: 0
     };
 
     it('should be a function', () => {
@@ -49,14 +51,18 @@ describe('The Test App root reducer', () => {
             expect(actualState).to.equal(originalState);
         });
 
-        it('should add items to the list if called with the ADD_ITEM action type', () => {
+        it('should add items to the list by ID if called with the ADD_ITEM action type', () => {
             const addItemAction = {
                 type: 'ADD_ITEM',
                 text: 'test'
             };
 
             const expectedState = {
-                items: ['test']
+                items: [1],
+                itemsById: {
+                    1: {id: 1, text: 'test'}
+                },
+                lastItemId: 1
             };
 
             const actualState = testApp({}, addItemAction);
@@ -70,15 +76,19 @@ describe('The Test App root reducer', () => {
                 text: 'test'
             };
 
-            const originalState = {
-                items: ['original1', 'original2']
-            };
-
             const expectedState = {
-                items: ['original1', 'original2', 'test']
+                items: [1, 2, 3],
+                lastItemId: 3,
+                itemsById: {
+                    1: {id: 1, text: 'test'},
+                    2: {id: 2, text: 'test'},
+                    3: {id: 3, text: 'test'}
+                }
             };
 
-            const actualState = testApp(originalState, addItemAction);
+            const firstState = testApp(defaultState, addItemAction);
+            const secondState = testApp(firstState, addItemAction);
+            const actualState = testApp(secondState, addItemAction);
 
             expect(actualState).to.deep.equal(expectedState);
         });
@@ -96,6 +106,33 @@ describe('The Test App root reducer', () => {
             deepFreeze(originalState); //will throw an exception if anyone tries to mutate originalState
 
             testApp(originalState, addItemAction);
+        });
+    });
+
+    describe('tracking item IDs', () => {
+        it('should keep track of the last ID that was used', () => {
+            expect(defaultState.lastItemId).to.equal(0);
+        });
+
+        it('should increment the IDs of subsequent items that are added', () => {
+            const addItemAction = {
+                type: 'ADD_ITEM',
+                text: 'test'
+            };
+
+            const expectedState = {
+                items: [1, 2],
+                itemsById: {
+                    1: {id: 1, text: 'test'},
+                    2: {id: 2, text: 'test'}
+                },
+                lastItemId: 2
+            };
+
+            const nextState = testApp(defaultState, addItemAction);
+            const actualState = testApp(nextState, addItemAction);
+
+            expect(actualState).to.deep.equal(expectedState);
         });
     });
 });
